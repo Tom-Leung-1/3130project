4083369afe282da8689ed19c8350746711d4e67c
Tom-Leung-bb <wfleung8@cse.cuhk.edu.hk>
2021-05-05 22:49:37 +0800

    init

---
+++ TraBlog_3310/app/src/main/java/edu/cuhk/csci3310/trablog_3310/data/LoginDataSource.java
@@ -0,0 +1,29 @@
+package edu.cuhk.csci3310.trablog_3310.data;
+
+import edu.cuhk.csci3310.trablog_3310.data.model.LoggedInUser;
+
+import java.io.IOException;
+
+/**
+ * Class that handles authentication w/ login credentials and retrieves user information.
+ */
+public class LoginDataSource {
+
+    public Result<LoggedInUser> login(String username, String password) {
+
+        try {
+            // TODO: handle loggedInUser authentication
+            LoggedInUser fakeUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            "Jane Doe");
+            return new Result.Success<>(fakeUser);
+        } catch (Exception e) {
+            return new Result.Error(new IOException("Error logging in", e));
+        }
+    }
+
+    public void logout() {
+        // TODO: revoke authentication
+    }
+}
---
+++ TraBlog_3310/app/src/main/java/edu/cuhk/csci3310/trablog_3310/data/model/LoggedInUser.java
@@ -0,0 +1,23 @@
+package edu.cuhk.csci3310.trablog_3310.data.model;
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+public class LoggedInUser {
+
+    private String userId;
+    private String displayName;
+
+    public LoggedInUser(String userId, String displayName) {
+        this.userId = userId;
+        this.displayName = displayName;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
