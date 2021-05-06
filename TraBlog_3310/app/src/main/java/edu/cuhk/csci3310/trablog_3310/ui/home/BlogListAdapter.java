9b6798ca2e90c7157c2345e3f8db1caedd22934f
Tom-Leung-bb <wfleung8@cse.cuhk.edu.hk>
2021-05-06 11:24:52 +0800

    blog lists prototype

---
+++ TraBlog_3310/app/src/main/java/edu/cuhk/csci3310/trablog_3310/ui/home/BlogListAdapter.java
@@ -0,0 +1,71 @@
+package edu.cuhk.csci3310.trablog_3310.ui.home;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+import edu.cuhk.csci3310.trablog_3310.R;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BlogListAdapter extends Adapter<BlogListAdapter.BlogViewHolder>{
+    private RecyclerViewClickListener listener;
+    private Fragment fragment;
+    private LayoutInflater mInflater;
+    private final LinkedList<String> mTitleList;
+    private final LinkedList<String> mDescList;
+
+    class BlogViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView titleView, descriptionView;
+        final BlogListAdapter mAdapter;
+
+        public BlogViewHolder(View itemView, BlogListAdapter adapter) {
+            super(itemView);
+            titleView = itemView.findViewById(R.id.title);
+            descriptionView = itemView.findViewById(R.id.description);
+            descriptionView.setOnClickListener(this);
+            titleView.setOnClickListener(this);
+            this.mAdapter = adapter;
+        }
+        @Override
+        public void onClick(View v) {
+            listener.onClick(v, getAdapterPosition());
+        }
+    }
+    public BlogListAdapter(Fragment fragment, LinkedList<String> titleList, LinkedList<String> descList, RecyclerViewClickListener listener) {
+        this.fragment = fragment;
+        mInflater = LayoutInflater.from(fragment.getActivity());
+        this.mTitleList = titleList;
+        this.mDescList = descList;
+        this.listener = listener;
+    }
+    public interface RecyclerViewClickListener{
+        void onClick(View v, int position);
+    }
+    @NonNull
+    @Override
+    public BlogViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mItemView = mInflater.inflate(R.layout.blog_list_item, parent, false);
+        return new BlogViewHolder(mItemView, this);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull BlogViewHolder holder, int position) {
+        String title = mTitleList.get(position);
+        final String desc = mDescList.get(position);
+        holder.titleView.setText(title);
+        holder.descriptionView.setText(desc);
+    }
+    public long getItemId(int position) {
+        return position;
+    }
+    @Override
+    public int getItemCount() {
+        return mTitleList.size();
+    }
+}
--- TraBlog_3310/app/src/main/java/edu/cuhk/csci3310/trablog_3310/ui/home/HomeFragment.java
+++ TraBlog_3310/app/src/main/java/edu/cuhk/csci3310/trablog_3310/ui/home/HomeFragment.java
@@ -1,35 +1,66 @@
 package edu.cuhk.csci3310.trablog_3310.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.LinkedList;
 
 import edu.cuhk.csci3310.trablog_3310.R;
 
 public class HomeFragment extends Fragment {
-
+    private RecyclerView mRecyclerView;
+    private BlogListAdapter mAdapter;
+    private BlogListAdapter.RecyclerViewClickListener mlistener;
     private HomeViewModel homeViewModel;
+    private LinkedList<String> titleList = new java.util.LinkedList<>();
+    private LinkedList<String> descList = new java.util.LinkedList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+//        homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
+
         return root;
     }
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mRecyclerView = getView().findViewById(R.id.recyclerview);
+        mlistener = new BlogListAdapter.RecyclerViewClickListener() {
+            @Override
+            public void onClick(View v, int position) { //test
+                Toast.makeText(getActivity(),"Hello ",Toast.LENGTH_SHORT).show();
+            }
+        };
+        //test
+        for (int i = 0; i < 50; i++){
+            titleList.add("ABC");titleList.add("DEF");
+            descList.add("123");descList.add("456");
+        }
+        mAdapter = new BlogListAdapter(this, titleList, descList, mlistener);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
 }
